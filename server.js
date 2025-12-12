6ab91b652476b35f259b9f1bcddc7004d1c1b996
vegaleksandr-coder <vegaleksandr@gmail.com>
2025-12-12 15:47:46 +0300

    Update server.js
    Fix Supabase key

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Инициализируем Supabase
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    process.env.SUPABASE_KEY
 );
 
 // Middleware
